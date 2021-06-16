7cb4238f49551ad33a7d2a051072377ef3a3954b
Sven <Svenn.knappmann@gmail.com>
2021-06-16 10:39:43 +0200

    added all assignments of H02

---
+++ H02/Opdracht2.php
@@ -0,0 +1,6 @@
+<?php
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo nl2br("M" . $b . "s" . $b . " " . $a . $b . " mak" . $b . ", werk" . $b . " " . $c . ". \n<a href='https://www.ikgastarten.nl/bedrijf-starten/de-start-van-klm'>-Albert Plasman (KLM)</a>");
