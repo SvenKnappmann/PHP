3f7954ed0e2ec95d9b6f5ded56681f2aed768e63
Sven <Svenn.knappmann@gmail.com>
2021-06-17 10:42:28 +0200

    Added H03, OP01.

--- H03/Opdracht1.php
+++ H03/Opdracht1.php
@@ -1,4 +1,14 @@
 <?php
-for ($i = 0; $i < 10; $i++) {
-    echo "<img src='img/aap1.jpg' alt='Monke'>";
+
+$array = array();
+
+for ($i = 1; $i < 11; $i++) {
+    if ($i < 10) {
+        $array[$i] = "<img src='img/aap" . $i . ".jpg' alt='Monke'>";
+        echo $array[$i];
+    }
+    else
+    {
+        echo "<img src='img/redhead_11.jpg' alt='Monke'>";
+    }
 }
