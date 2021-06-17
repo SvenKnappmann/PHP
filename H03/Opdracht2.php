59e04f7af1374584b7676286bee1c5ccc3c93f95
Sven <Svenn.knappmann@gmail.com>
2021-06-17 11:21:42 +0200

    Added H03, OP02.

--- H03/Opdracht2.php
+++ H03/Opdracht2.php
@@ -1,1 +1,11 @@
 <?php
+
+$star = array("*");
+echo "<div style='width: 200px; text-align: center'>";
+for ($i = 1; $i < 10; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo " * ";
+    }
+    echo "<br>";
+}
+echo "</div>";
