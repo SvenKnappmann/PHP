6c9c45332988fe06a6437729472b801a67652393
Sven <Svenn.knappmann@gmail.com>
2021-06-21 14:19:13 +0200

    Added H03, OP03 and OP04.

--- H03/Opdracht4.php
+++ H03/Opdracht4.php
@@ -1,1 +1,16 @@
 <?php
+
+$array = array();
+$color = "green";
+
+echo "<div style='text-align: center'>";
+for ($i = 1; $i < 10; $i++) {
+    $array[$i] = "<img src='img/aap" . $i . ".jpg' alt='Monke' style='border: solid " . $color . " 5px; width: 270px; height: 270px; margin-bottom: 5px'>";
+    echo $array[$i];
+    if ($i % 2 == 0) {
+        $color = "green";
+    } else {
+        $color = "red";
+    }
+}
+echo "</div>";
