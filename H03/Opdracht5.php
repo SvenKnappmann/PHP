25fe0c37c10e90c8cd4083b3a2eecbfb9c8aa292
Sven <Svenn.knappmann@gmail.com>
2021-06-22 11:27:08 +0200

    Changed H03,OP05. Change in efficiency.

--- H03/Opdracht5.php
+++ H03/Opdracht5.php
@@ -1,13 +1,78 @@
 <?php
 
-$leeftijd = 88;
-$bedrag = null;
-
-if ($leeftijd > 12 && $leeftijd <= 65) {
-    $bedrag = 10;
-} elseif ($leeftijd < 3) {
-    $bedrag = 0;
-} else {
-    $bedrag = 5;
+$leeftijden = [10, 2, 65, 12];
+$array = [4, 5,6 ,7,8];
+showPrice($leeftijden);
+showPrice($array);
+function showPrice($ages){
+    for ($i = 0; $i < count($ages); $i++) {
+        echo calcPrice($ages[$i]);
+        echo "<br>";
+    }
 }
-echo $bedrag;
+function calcPrice($input)
+{
+
+    if ($input > 12 && $input <= 65) {
+        $bedrag = 10;
+    } elseif ($input < 3) {
+        $bedrag = 0;
+    } else {
+        $bedrag = 5;
+    }
+    return $bedrag;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//return ($input > 12 && $input <= 65) ? 10 : ($input < 3 ? 0 : 5);
