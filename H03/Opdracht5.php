99fba970aac27494711d1584dffb885bef0c87f6
Sven <Svenn.knappmann@gmail.com>
2021-06-22 08:43:20 +0200

    Added H03, OP05.

--- H03/Opdracht5.php
+++ H03/Opdracht5.php
@@ -1,1 +1,13 @@
 <?php
+
+$leeftijd = 88;
+$bedrag = null;
+
+if ($leeftijd > 12 && $leeftijd <= 65) {
+    $bedrag = 10;
+} elseif ($leeftijd < 3) {
+    $bedrag = 0;
+} else {
+    $bedrag = 5;
+}
+echo $bedrag;
