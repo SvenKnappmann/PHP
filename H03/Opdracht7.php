2e2ff8cd013754dc65e581dc02a8f695fa3153e0
Sven <Svenn.knappmann@gmail.com>
2021-06-22 10:50:42 +0200

    Added H03, OP07.

--- H03/Opdracht7.php
+++ H03/Opdracht7.php
@@ -1,1 +1,19 @@
 <?php
+
+$kappersagenda = [
+    "9.15" => "Mevr. Pietersen",
+    "9.30" => "Mevr. Willems",
+    "9.45" => "",
+    "10.00" => "Mevr. Pietersen",
+    "10.15" => "Mevr. Willems",
+    "10.30" => "",
+];
+
+
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach($kappersagenda as $tijd => $afspraak) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>") ;
+    }
+}
+print ("</ul>");
