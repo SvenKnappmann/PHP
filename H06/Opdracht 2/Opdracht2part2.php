3660ccc6d4c542dbef19983cd79011f08dcf014e
Sven <Svenn.knappmann@gmail.com>
2021-06-29 10:25:23 +0200

    changed H06

--- H06/Opdracht 2/Opdracht2part2.php
+++ H06/Opdracht 2/Opdracht2part2.php
@@ -1,16 +1,34 @@
 <?php
-$logins = [
-    "piet@worldonline.nl" => "doetje123",
-    "klaas@carpets.nl" => "snoepje777",
-    "truushendriks@wegweg.nl" => "arkiearkie201"
-];
+//$logins = [
+//    "piet@worldonline.nl" => "doetje123",
+//    "klaas@carpets.nl" => "snoepje777",
+//    "truushendriks@wegweg.nl" => "arkiearkie201"
+//];
 
-checkCorrectLogin($_POST['email'], $_POST['password'], $logins);
+$host = 'localhost';
+$port = '3306';
+$user = 'root';
+$pass = '';
+$db = 'phpschool';
 
-function checkCorrectLogin($emailUser, $password, $logins){
-    if(array_key_exists($emailUser, $logins) && $logins[$emailUser] === $password){
-        echo "Welkom";
-    } else {
-        echo "Sorry, geen toegang";
+$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';port=' . $port, $user, $pass);
+
+$accounts = $dbh->query("SELECT * FROM account");
+
+$loginResult = checkCorrectLogin($_POST['email'], $_POST['password'], $accounts);
+
+if ($loginResult) {
+    echo "Welkom";
+} else {
+    echo "Sorry, geen toegang";
+}
+
+function checkCorrectLogin($emailUser, $password, $accounts) {
+    foreach ($accounts as $account) {
+        if ($account["email"] === $emailUser && $account["password"] == $password) {
+            return true;
+        }
     }
+
+    return false;
 }
